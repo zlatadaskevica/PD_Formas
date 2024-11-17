58032c6950bc91456e0e000d7635ee0524934d6d
zlatadaskevica <dskvc.z@inbox.lv>
2024-11-17 10:13:02 +0200

    Add files via upload

--- pdformas/login.php
+++ pdformas/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ĶLUS - Pieslēgties</title>
+    <title>Pieslēgties</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
